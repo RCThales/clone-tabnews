59efa3d7732232d68a877884f4a98f635793a1b5
Thales Cardoso <46228954+RCThales@users.noreply.github.com>
2024-01-29 02:45:13 +0000

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-const Home = () =>{
-    return <h1 className="test">O primeiro trabalho vai vir!</h1>
-}
+const Home = () => {
+  return <h1 className="test">O primeiro trabalho vai vir!</h1>;
+};
 
 export default Home;
