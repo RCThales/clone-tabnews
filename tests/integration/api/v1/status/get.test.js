4acdfd6193ef3a86c72cfa14025b8407d2c5ff76
Thales Cardoso <46228954+RCThales@users.noreply.github.com>
2024-02-27 01:41:04 +0000

    Add `compose` config file with database service

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,3 +3,9 @@
   
   expect(response.status).toBe(200);
 })
+
+test("GET to /api/v1/status should return 404", async () => {
+  const response = await fetch(("http://localhost:3000/api/v1/JABURU"));
+  
+  expect(response.status).toBe(404);
+})
